13a5c0257cb7cff2b235e14fe0ab42fcb0e81eb6
Vlera Shehu <vs48268@ubt-uni.net>
2021-04-08 01:39:59 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,14 @@ namespace API
             
                      opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+               services.AddCors(opt =>
+               {
+                   opt.AddPolicy("CorsPolicy", policy => 
+                   {
+                       policy.AllowAnyHeader().AllowAnyMethod().WithOrigins
+                       ("http://localhost:3000");
+                   });
+               } );
                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             
             
@@ -45,6 +53,7 @@ namespace API
         {
             if (env.IsDevelopment())
             {
+
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -54,6 +63,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
             app.UseMvc();
         }
     }
