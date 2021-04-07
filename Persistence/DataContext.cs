4946c11075dcd495429e8bf626a5992ffce5d73f
Vlera Shehu <vs48268@ubt-uni.net>
2021-04-07 15:30:27 +0200

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+
+namespace Persistence
+{
+  
+        public class DataContext: DbContext
+        {
+            
+            public DataContext(DbContextOptions options ): base(options)
+            {
+            }
+            public DbSet<Value> Values {get; set;}
+
+            protected override void OnModelCreating(ModelBuilder builder)
+            {
+                builder.Entity<Value>()
+                    .HasData(
+                        new Value{id = 1, Name = "Value Anna"},
+                        new Value{id = 2, Name = "Value Nicole"},
+                        new Value{id = 3, Name = "Value Bob"}
+                    );
+
+            }
+        }
+}
